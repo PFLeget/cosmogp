344ba15302f850fc086f6f4fb9d3fbb2aee132a0
Pierre-Francois Leget <leget@clermont.in2p3.fr>
2017-04-14 15:56:43 -0700

    I add 2D RBF kernel inside kernel.py

--- cosmogp/kernel.py
+++ cosmogp/kernel.py
@@ -15,21 +15,22 @@ def RBF_kernel_1D(x,hyperparameter,nugget,floor=0.00,y_err=None):
     K(x_i,x_j) = sigma^2 exp(-0.5 ((x_i-x_j)/l)^2) 
                + (y_err[i]^2 + nugget^2 + floor^2) delta_ij
 
+    sigma = hyperparameter[0]
+    l = hyperparameter[1]
+
+    sigma -->  Kernel amplitude hyperparameter. 
+    It explain the standard deviation from the mean function.
+
+    l --> Kernel correlation length hyperparameter. 
+    It explain at wich scale one data point afect the 
+    position of an other.
+
+
     
     x : 1D numpy array or 1D list. Grid of observation.
     For SNIa it would be observation phases. 
 
-    hyperparameter : dic. dictionnary that have as key sigma 
-    and l wich are the two parameters of th RBF kernel.
-    hyperparameter.keys() should return {'sigma':float,
-    'l': float}
-
-    sigma : float. Kernel amplitude hyperparameter. 
-    It explain the standard deviation from the mean function.
-
-    l : float. Kernel correlation length hyperparameter. 
-    It explain at wich scale one data point afect the 
-    position of an other.
+    hyperparameter : 1D numpy array or 1D list. 
 
     nugget : float. Diagonal dispertion that you can add in 
     order to explain intrinsic variability not discribe by 
@@ -59,6 +60,67 @@ def RBF_kernel_1D(x,hyperparameter,nugget,floor=0.00,y_err=None):
                 Cov[i,j] += y_err[i]**2+floor**2+nugget**2
 
     return Cov
+
+
+
+def RBF_kernel_2D(x,hyperparameter,nugget,floor=0.00,y_err=None):
+
+    """
+    2D RBF kernel
+
+    K(x_i,x_j) = sigma^2 exp(-0.5 (x_i-x_j)^t L (x_i-x_j)) 
+               + (y_err[i]^2 + nugget^2 + floor^2) delta_ij
+
+    sigma = hyperparameter[0]
+
+    L = numpy.array(([hyperparameter[1]**2,hyperparameter[3]],
+                   [hyperparameter[3],hyperparameter[2]**2]))
+    
+    sigma --> Kernel amplitude hyperparameter. 
+    It explain the standard deviation from the mean function.
+
+    L --> Kernel correlation length hyperparameter. 
+    It explain at wich scale one data point afect the 
+    position of an other.
+
+    x : 2D numpy array or 2D list. Grid of coordinate.
+    For WL it would be pixel coordinate. 
+
+    hyperparameter : 1D numpy array or 1D list. 
+
+    nugget : float. Diagonal dispertion that you can add in 
+    order to explain intrinsic variability not discribe by 
+    the RBF kernel.
+
+    floor : float. Diagonal error that you can add to your 
+    RBF Kernel if you know the value of your intrinsic dispersion. 
+
+    y_err : 1D numpy array or 1D list. Error from data 
+    observation. For WL, it would be the error on the 
+    parameter that you want to interpolate in the focal plane.
+
+
+    output : Cov. 2D numpy array, shape = (len(x),len(x))
+    """
+    
+    if y_err is None:
+        y_err = N.zeros(len(x))
+    
+    Cov = N.zeros((len(x),len(x)))
+    L=N.array(([hyperparameter[1]**2,hyperparameter[3]],
+               [hyperparameter[3],hyperparameter[2]**2]))
+    
+    for i in range(len(x)):
+        for j in range(len(x)):
+            delta_x = x[i]-x[j]
+            delta_x_t = delta_x.reshape((len(delta_x),1))
+            Cov[i,j] = (hyperparameter[0]**2)*N.exp(-0.5*delta_x.dot(N.dot(L,delta_x_t)))
+            
+            if i==j:
+                Cov[i,j] += y_err[i]**2+floor**2+nugget**2
+
+    return Cov
+
 
 
 
