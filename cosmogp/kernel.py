502545c19da87f8a26dc9e3434b87446feaad501
PFLeget <leget@stanford.edu>
2017-05-02 18:43:11 -0700

    remove loop (put in option for the moment) in order to accelerate Kernel computation (I should remove SPEED option later)

--- cosmogp/kernel.py
+++ cosmogp/kernel.py
@@ -5,7 +5,7 @@ import numpy as N
 #TO DO : each times I should implement the H matrix which will be the
 # the covariance matrix and the new binning (to implement in 1D and 2D)
 
-def rbf_kernel_1d(x, hyperparameter, nugget, floor=0.00, y_err=None):
+def rbf_kernel_1d(x, hyperparameter, nugget, floor=0.00, y_err=None,SPEED=False):
     """
     1D RBF kernel.
 
@@ -45,37 +45,57 @@ def rbf_kernel_1d(x, hyperparameter, nugget, floor=0.00, y_err=None):
     """
     if y_err is None:
         y_err = N.zeros_like(x)
-    
-    Cov = N.zeros((len(x),len(x)))
-    
-    for i in range(len(x)):
-        for j in range(len(x)):
-            Cov[i,j] = (hyperparameter[0]**2)*N.exp(-0.5*((x[i]-x[j])/hyperparameter[1])**2)
-            
-            if i==j:
-                Cov[i,j] += y_err[i]**2+floor**2+nugget**2
 
+    if not SPEED :
+        Cov = N.zeros((len(x),len(x)))
+        
+        for i in range(len(x)):
+            for j in range(len(x)):
+                Cov[i,j] = (hyperparameter[0]**2)*N.exp(-0.5*((x[i]-x[j])/hyperparameter[1])**2)
+            
+                if i==j:
+                    Cov[i,j] += y_err[i]**2+floor**2+nugget**2
+    else:
+        A = x-x[:,None]
+
+        Cov = (hyperparameter[0]**2)*N.exp(-0.5*((A*A)/(hyperparameter[1]**2)))
+        Cov += (N.eye(len(y_err))*(y_err**2+floor**2+nugget**2))
+        
     return Cov
 
 
-def compute_rbf_1d_ht_matrix(new_x,old_x,hyperparameters,as_the_same_grid=False):
+def compute_rbf_1d_ht_matrix(new_x,old_x,hyperparameters,as_the_same_grid=False,SPEED=False):
     
     HT=[]
-    
-    for sn in range(len(old_x)): 
-        if as_the_same_grid:
-            New_binning=new_x[sn]
-        else:
-            New_binning=new_x
 
-        HT.append(N.zeros((len(New_binning),len(old_x[sn]))))
-        for i in range(len(New_binning)):
-            for j in range(len(old_x[sn])):
-                HT[sn][i,j]=(hyperparameters[0]**2)*N.exp(-0.5*((New_binning[i]-old_x[sn][j])/hyperparameters[1])**2)
+    if not SPEED:
+        for sn in range(len(old_x)): 
+            if as_the_same_grid:
+                New_binning=new_x[sn]
+            else:
+                New_binning=new_x
+            
+            HT.append(N.zeros((len(New_binning),len(old_x[sn]))))
+            for i in range(len(New_binning)):
+                for j in range(len(old_x[sn])):
+                    HT[sn][i,j]=(hyperparameters[0]**2)*N.exp(-0.5*((New_binning[i]-old_x[sn][j])/hyperparameters[1])**2)
+
+    else:
+        for sn in range(len(old_x)): 
+            if as_the_same_grid:
+                New_binning=new_x[sn]
+            else:
+                New_binning=new_x
+
+            #A = New_binning-old_x[sn][:,None]
+            A = old_x[sn]-New_binning[:,None]
+            HT.append((hyperparameters[0]**2)*N.exp(-0.5*((A*A)/(hyperparameters[1]**2))))
+        
 
     return HT
 
-def rbf_kernel_2d(x,hyperparameter,nugget,floor=0.00,y_err=None):
+
+def rbf_kernel_2d(x,hyperparameter,nugget,floor=0.00,y_err=None,SPEED=False):
     """
     2D RBF kernel.
 
@@ -115,56 +135,80 @@ def rbf_kernel_2d(x,hyperparameter,nugget,floor=0.00,y_err=None):
     """
     if y_err is None:
         y_err = N.zeros(len(x))
-    
-    #Cov = N.zeros((len(x),len(x)))
-    #L=N.array(([hyperparameter[1]**2,hyperparameter[3]],
-    #           [hyperparameter[3],hyperparameter[2]**2]))
-    
-    #Inv_L=N.linalg.inv(L)
-    
-    Inv_L=N.array(([hyperparameter[2]**2,-hyperparameter[3]],
-                   [-hyperparameter[3],hyperparameter[1]**2]))
-    
-    Inv_L*=1./(((hyperparameter[1]**2)*(hyperparameter[2]**2))-hyperparameter[3]**2)
-    
-    #for i in range(len(x)):
-    #    for j in range(len(x)):
-    #        delta_x = x[i]-x[j]
-    #        delta_x_t = delta_x.reshape((len(delta_x),1))
-    #        Cov[i,j] = (hyperparameter[0]**2)*N.exp(-0.5*delta_x.dot(N.dot(Inv_L,delta_x_t)))
-    #        
-    #        if i==j:
-    #            Cov[i,j] += y_err[i]**2+floor**2+nugget**2
 
-    A = (x[:,0]-x[:,0][:,None])
-    B = (x[:,1]-x[:,1][:,None])
+    if not SPEED:
+        Cov = N.zeros((len(x),len(x)))
+        L=N.array(([hyperparameter[1]**2,hyperparameter[3]],
+                   [hyperparameter[3],hyperparameter[2]**2]))
+    
+        Inv_L=N.linalg.inv(L)
 
-    Cov = (hyperparameter[0]**2)*N.exp(-0.5*(A*A*Inv_L[0,0] + 2.*A*B*Inv_L[0,1] + B*B*Inv_L[1,1]))
-    Cov += (N.eye(len(y_err))*y_err**2)+floor**2+nugget**2 
+        for i in range(len(x)):
+            for j in range(len(x)):
+                delta_x = x[i]-x[j]
+                delta_x_t = delta_x.reshape((len(delta_x),1))
+                Cov[i,j] = (hyperparameter[0]**2)*N.exp(-0.5*delta_x.dot(N.dot(Inv_L,delta_x_t)))
+                    
+                if i==j:
+                    Cov[i,j] += y_err[i]**2+floor**2+nugget**2
+
+    else:
+        Inv_L=N.array(([hyperparameter[2]**2,-hyperparameter[3]],
+                       [-hyperparameter[3],hyperparameter[1]**2]))
+    
+        Inv_L*=1./(((hyperparameter[1]**2)*(hyperparameter[2]**2))-hyperparameter[3]**2)
+    
+        A = (x[:,0]-x[:,0][:,None])
+        B = (x[:,1]-x[:,1][:,None])
+
+        Cov = (hyperparameter[0]**2)*N.exp(-0.5*(A*A*Inv_L[0,0] + 2.*A*B*Inv_L[0,1] + B*B*Inv_L[1,1]))
+        Cov += (N.eye(len(y_err))*(y_err**2+floor**2+nugget**2))
     
     return Cov
 
 
-def compute_rbf_2d_ht_matrix(new_x,old_x,hyperparameters,as_the_same_grid=False):
-    
+def compute_rbf_2d_ht_matrix(new_x,old_x,hyperparameters,as_the_same_grid=False,SPEED=False):
+
     HT=[]
-    L=N.array(([hyperparameters[1]**2,hyperparameters[3]],
-               [hyperparameters[3],hyperparameters[2]**2]))
+    if not SPEED:
 
-    Inv_L=N.linalg.inv(L)
+        L=N.array(([hyperparameters[1]**2,hyperparameters[3]],
+                   [hyperparameters[3],hyperparameters[2]**2]))
+
+        Inv_L=N.linalg.inv(L)
     
-    for sn in range(len(old_x)): 
-        if as_the_same_grid:
-            New_binning=new_x[sn]
-        else:
-            New_binning=new_x
+        for sn in range(len(old_x)): 
+            if as_the_same_grid:
+                New_binning=new_x[sn]
+            else:
+                New_binning=new_x
 
-        HT.append(N.zeros((len(New_binning),len(old_x[sn]))))
-        for i in range(len(New_binning)):
-            for j in range(len(old_x[sn])):
-                delta_x = New_binning[i]-old_x[sn][j]
-                delta_x_t = delta_x.reshape((len(delta_x),1))
-                HT[sn][i,j]=(hyperparameters[0]**2)*N.exp(-0.5*delta_x.dot(N.dot(Inv_L,delta_x_t)))
+            HT.append(N.zeros((len(New_binning),len(old_x[sn]))))
+            for i in range(len(New_binning)):
+                for j in range(len(old_x[sn])):
+                    delta_x = New_binning[i]-old_x[sn][j]
+                    delta_x_t = delta_x.reshape((len(delta_x),1))
+                    HT[sn][i,j]=(hyperparameters[0]**2)*N.exp(-0.5*delta_x.dot(N.dot(Inv_L,delta_x_t)))
+                
+    else:
+        for sn in range(len(old_x)): 
+            if as_the_same_grid:
+                New_binning = new_x[sn]
+            else:
+                New_binning = new_x
+        
+            Inv_L = N.array(([hyperparameters[2]**2,-hyperparameters[3]],
+                             [-hyperparameters[3],hyperparameters[1]**2]))
+    
+            Inv_L *= 1./(((hyperparameters[1]**2)*(hyperparameters[2]**2))-hyperparameters[3]**2)
+    
+            A = (old_x[sn][:,0]-New_binning[:,0][:,None])
+            B = (old_x[sn][:,1]-New_binning[:,1][:,None])
+
+            HT.append((hyperparameters[0]**2)*N.exp(-0.5*(A*A*Inv_L[0,0] + 2.*A*B*Inv_L[0,1] + B*B*Inv_L[1,1])))
+
+    
+
 
     return HT
 
