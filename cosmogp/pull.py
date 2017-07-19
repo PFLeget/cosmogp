b8ba8e39ab2594b770c7eb5c3ad2d398c46b7f1e
PFLeget <leget@stanford.edu>
2017-07-19 16:03:23 -0700

    improvement style pull.py

--- cosmogp/pull.py
+++ cosmogp/pull.py
@@ -7,13 +7,12 @@ from scipy.stats import norm as normal
 
 class build_pull:
 
-    def __init__(self,y,x,hyperparameters,
-                 nugget=0.,y_err=None,y_mean=None,
-                 x_axis_mean=None,kernel='RBF1D'):
-
+    def __init__(self, y, x, hyperparameters,
+                 nugget=0., y_err=None, y_mean=None,
+                 x_axis_mean=None, kernel='RBF1D'):
         """
         Build the pull of GP interpolation. 
-        
+
         Basically it will return you the pull and 
         the residual for a given kernel and a given 
         set of hyperparameter(s). 
@@ -29,7 +28,7 @@ class build_pull:
 
         self.kernel = kernel
         self.nugget = nugget
-        
+
         self.n_object = len(y)
 
         self._pull = []
@@ -39,58 +38,56 @@ class build_pull:
         self.pull_average = None
         self.pull_std = None
 
-        
-    def compute_pull(self,diff=None,svd=True,
-                     substract_mean=False):
 
+    def compute_pull(self, diff=None, svd=True,
+                     substract_mean=False):
         """
         Function that compute the pull.
 
         Will return pull and residual
         """
-        
+
         for sn in range(self.n_object):
 
             pred = np.zeros(len(self.y[sn]))
             pred_var = np.zeros(len(self.y[sn]))
-            
+
             for t in range(len(self.y[sn])):
-                
+
                 FILTRE = np.array([True]*len(self.y[sn]))
                 FILTRE[t] = False
-                
+
                 if self.y_err is None:
                     yerr = np.zeros_like(self.y[sn])
                 else:
                     yerr = self.y_err[sn]
-                    
+
                 gpp = cosmogp.gaussian_process(self.y[sn][FILTRE],
                                                self.x[sn][FILTRE],
                                                y_err=yerr[FILTRE],
                                                Mean_Y=self.y_mean,
                                                Time_mean=self.x_axis_mean,
                                                kernel=self.kernel)
-                
+
                 if diff or substract_mean:
                     if diff is None:
                         gpp.substract_Mean(diff=None)
                     else:
                         gpp.substract_Mean(diff=[diff[sn]])
-                                
+
                 gpp.hyperparameters = self.hyperparameters
                 gpp.nugget = self.nugget
                 gpp.get_prediction(new_binning=self.x[sn],SVD=svd)
-                
+
                 pred[t] = gpp.Prediction[0][t]
                 pred_var[t] = abs(gpp.covariance_matrix[0][t,t])
 
-            self.gpp = gpp
-            
+
             self.pred_var = pred_var
             pull = (pred-self.y[sn])/np.sqrt(yerr**2+pred_var+self.nugget**2)
             res = pred-self.y[sn]
             self._pull.append(pull)
-            
+
             for t in range(len(self.y[sn])):
                 self.pull.append(pull[t])
                 self.residual.append(res[t])
@@ -98,8 +95,7 @@ class build_pull:
         self.pull_average,self.pull_std = normal.fit(self.pull)
 
 
-    def plot_result(self,binning=60):
-
+    def plot_result(self, binning=60):
         """
         Plot the pull distribution. 
 
@@ -113,9 +109,9 @@ class build_pull:
         """
 
         import pylab as plt
-        
+
         plt.figure()
-        
+
         plt.hist(self.pull,bins=binning,normed=True)
 
         xmin, xmax = plt.xlim()
@@ -124,15 +120,14 @@ class build_pull:
         xmin, xmax = plt.xlim()
         xaxis = np.linspace(xmin, xmax, 100)
         pdf = normal.pdf(xaxis, self.pull_average, self.pull_std)
-        
+
         plt.plot(xaxis, pdf, 'r', linewidth=3)
 
-        title = r"Fit results: $\mu$ = $%.2f\pm%.2f$, $\sigma$ = $%.2f\pm%.2f$" % (self.pull_average,
-                                                                                   self.pull_std/np.sqrt(len(self.pull)),
-                                                                                   self.pull_std,
-                                                                                   self.pull_std/np.sqrt(2*len(self.pull)))
-        
+        title = r"Fit results: $\mu$ = $%.2f\pm%.2f$,"% (self.pull_average,
+                                                         self.pull_std/np.sqrt(len(self.pull)))
+        title += r"$\sigma$ = $%.2f\pm%.2f$"%(self.pull_std,
+                                              self.pull_std/np.sqrt(2*len(self.pull)))
+
         plt.title(title)
         plt.ylabel('Number of points (normed)')
         plt.xlabel('Pull')
-
