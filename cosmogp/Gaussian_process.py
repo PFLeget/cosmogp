I remove useless function in kernel and fixed a bug for taking into account average function

cosmogp/Gaussian_process.py
@@ -51,9 +51,10 @@ def log_likelihood_gp(y, x_axis, kernel, hyperparameter, nugget,
         y_mean = np.zeros_like(y)
 
     number_points = len(x_axis)
-    kernel_matrix = kernel(x_axis, hyperparameter, nugget, y_err=y_err)
+    kernel_matrix = kernel(x_axis, hyperparameter, nugget=nugget, y_err=y_err)
     y_ket = y.reshape(len(y), 1)
-    y_mean_colomn = y_mean.reshape(len(y_mean), 1)
+    y_mean_colomn = np.ones(len(y))*y_mean
+    y_mean_colomn = y_mean_colomn.reshape(len(y_mean_colomn), 1)
 
     if svd_method:  #svd decomposition
         inv_kernel_matrix, log_det_kernel_matrix = svd(kernel_matrix,
@@ -136,21 +137,17 @@ class Gaussian_process:
 
         if kernel == 'RBF1D':
             from cosmogp import rbf_kernel_1d as kernel
-            from cosmogp import compute_rbf_1d_ht_matrix as compute_ht
             from cosmogp import init_rbf as init_hyperparam
 
             self.kernel = kernel
-            self.compute_HT_matrix = compute_ht
             sigma, L = init_hyperparam(Time,y)
             self.hyperparameters = np.array([sigma, L])
 
         if kernel == 'RBF2D':
             from cosmogp import rbf_kernel_2d as kernel
-            from cosmogp import interpolate_mean_2d as interpolate_mean
             from cosmogp import init_rbf as init_hyperparam
 
             self.kernel = kernel
-            self.compute_HT_matrix = compute_ht
             sigma, L = init_hyperparam(Time,y)
             self.hyperparameters = np.array([sigma, L, L, 0.])
 
@@ -171,12 +168,14 @@ class Gaussian_process:
 
         self.Mean_Y = Mean_Y
         self.Time_mean = Time_mean
+        self.substract_mean = substract_mean
+        
         if diff is None:
             self.diff=np.array([None]*self.N_sn)
         else:
             self.diff = diff
 
-        if substract_mean or Mean_Y is not None:
+        if self.substract_mean or self.Mean_Y is not None:
             self.y0 = []
             for i in range(self.N_sn):
                 self.y0.append(return_mean(self.y[i], self.Time[i], mean_y=self.Mean_Y,
@@ -263,7 +262,7 @@ class Gaussian_process:
         for sn in range(self.N_sn):
 
             self.kernel_matrix.append(self.kernel(self.Time[sn], self.hyperparameters,
-                                      self.nugget, y_err=self.y_err[sn]))
+                                      nugget=self.nugget, y_err=self.y_err[sn]))
 
 
     def get_prediction(self, new_binning=None, COV=True, svd_method=True):
@@ -288,9 +287,7 @@ class Gaussian_process:
             self.new_binning = new_binning
 
         self.compute_kernel_matrix()
-        self.HT = self.compute_HT_matrix(self.new_binning, self.Time,
-                                         self.hyperparameters,
-                                         as_the_same_grid=self.as_the_same_time)
+
         self.Prediction = []
 
         for i in range(self.N_sn):
@@ -304,11 +301,15 @@ class Gaussian_process:
 
         for sn in range(self.N_sn):
 
-            if self.as_the_same_time:
-                new_y0 = self.y0[sn]
+            if self.substract_mean or self.Mean_Y is not None:
+            
+                if self.as_the_same_time:
+                    new_y0 = self.y0[sn]
+                else:
+                    new_y0 = return_mean(self.y[sn], self.Time[sn], new_x=self.new_binning,
+                                         mean_y=self.Mean_Y, mean_xaxis=self.Time_mean, diff=self.diff[sn])
             else:
-                new_y0 = return_mean(self.y[sn], self.Time[sn], new_x=self.new_binning,
-                                     mean_y=self.Mean_Y, mean_xaxis=self.Time_mean, diff=self.diff[sn])
+                new_y0 = 0.
 
             self.inv_kernel_matrix.append(np.zeros((len(self.Time[sn]), len(self.Time[sn]))))
             Y_ket = (self.y[sn] - self.y0[sn]).reshape(len(self.y[sn]), 1)
@@ -320,7 +321,14 @@ class Gaussian_process:
 
             self.inv_kernel_matrix[sn] = inv_kernel_matrix
 
-            self.Prediction[sn] += (np.dot(self.HT[sn],np.dot(inv_kernel_matrix,Y_ket))).T[0]
+            if not self.as_the_same_time:
+                new_grid = self.new_binning
+            else:
+                new_grid = self.new_binning[i]
+            
+            H = self.kernel(self.Time[sn], self.hyperparameters, new_x=new_grid)
+
+            self.Prediction[sn] += (np.dot(H,np.dot(inv_kernel_matrix,Y_ket))).T[0]
             self.Prediction[sn] += new_y0
 
         if COV:
@@ -337,14 +345,18 @@ class Gaussian_process:
 
         for sn in range(self.N_sn):
 
-            self.covariance_matrix.append(-np.dot(self.HT[sn], np.dot(self.inv_kernel_matrix[sn], self.HT[sn].T)))
-
             if self.as_the_same_time:
-                self.covariance_matrix[sn] += self.kernel(self.new_binning[sn],
-                                                          self.hyperparameters, 0)
+                new_grid = self.new_binning[sn]
             else:
-                self.covariance_matrix[sn]+=self.kernel(self.new_binning,
-                                                        self.hyperparameters, 0)
+                new_grid = self.new_binning
+
+            H = self.kernel(self.Time[sn], self.hyperparameters, new_x=new_grid)
+            K = self.kernel(new_grid, self.hyperparameters)
+            
+            self.covariance_matrix.append(-np.dot(H, np.dot(self.inv_kernel_matrix[sn], H.T)))
+
+            self.covariance_matrix[sn] += K
+
 
 
 class gaussian_process(Gaussian_process):

cosmogp/inv_matrix.py
@@ -1,8 +1,9 @@
+from scipy import linalg
 import numpy as np
 
 def svd_inverse(matrix,return_logdet=False):
 
-    U,s,V = np.linalg.svd(matrix)
+    U,s,V = linalg.svd(matrix)
     Filtre = (s>10**-15)
     if np.sum(Filtre)!=len(Filtre):
         print 'Pseudo-inverse decomposition :', len(Filtre)-np.sum(Filtre)
@@ -19,9 +20,9 @@ def svd_inverse(matrix,return_logdet=False):
     
 def cholesky_inverse(matrix,return_logdet=False):
 
-    L = np.linalg.cholesky(matrix)
-    inv_L = np.linalg.inv(L)
-    inv_matrix = np.dot(inv_L.T,inv_L)
+    L = linalg.cholesky(matrix, lower=True)
+    inv_L = linalg.inv(L)
+    inv_matrix = np.dot(inv_L.T, inv_L)
 
     if return_logdet:
         log_det=np.sum(2.*np.log(np.diag(L)))

cosmogp/kernel.py
@@ -1,27 +1,28 @@
 """implementation of different kind of kernel."""
 
-import numpy as N
+import numpy as np
 
 def init_rbf(x,y):
-    
-    number_point = N.zeros(len(y))
-    L_min = N.zeros(len(y))
-    L_max = N.zeros(len(y))
-    sigma = N.zeros(len(y))
-    
+
+    number_point = np.zeros(len(y))
+    L_min = np.zeros(len(y))
+    L_max = np.zeros(len(y))
+    sigma = np.zeros(len(y))
+
     for i in range(len(y)):
-        L_min = N.min(x[i])
-        L_max = N.max(x[i])
+        L_min = np.min(x[i])
+        L_max = np.max(x[i])
         number_point[i] = len(x[i])
-        sigma = N.std(y[i])
-    
-    d = N.mean(N.sqrt((L_max-L_min)**2/number_point))
-    L = N.mean(L_max-L_min)
+        sigma = np.std(y[i])
 
-    return N.mean(sigma), N.mean([d,L])
-    
+    d = np.mean(np.sqrt((L_max-L_min)**2/number_point))
+    L = np.mean(L_max-L_min)
 
-def rbf_kernel_1d(x, hyperparameter, nugget, floor=0.00, y_err=None):
+    return np.mean(sigma), np.mean([d,L])
+
+
+def rbf_kernel_1d(x, hyperparameter, new_x=None,
+                  nugget=0., floor=0.00, y_err=None):
     """
     1D RBF kernel.
 
@@ -34,58 +35,49 @@ def rbf_kernel_1d(x, hyperparameter, nugget, floor=0.00, y_err=None):
     sigma -->  Kernel amplitude hyperparameter. 
     It explain the standard deviation from the mean function.
 
-    l --> Kernel correlation length hyperparameter. 
-    It explain at wich scale one data point afect the 
+    l --> Kernel correlation length hyperparameter.
+    It explain at wich scale one data point afect the
     position of an other.
 
-
-    
     x : 1D numpy array or 1D list. Grid of observation.
-    For SNIa it would be observation phases. 
+    For SNIa it would be observation phases.
 
-    hyperparameter : 1D numpy array or 1D list. 
+    hyperparameter : 1D numpy array or 1D list.
 
-    nugget : float. Diagonal dispertion that you can add in 
-    order to explain intrinsic variability not discribe by 
+    nugget : float. Diagonal dispertion that you can add in
+    order to explain intrinsic variability not discribe by
     the RBF kernel.
 
-    floor : float. Diagonal error that you can add to your 
-    RBF Kernel if you know the value of your intrinsic dispersion. 
+    floor : float. Diagonal error that you can add to your
+    RBF Kernel if you know the value of your intrinsic dispersion.
 
-    y_err : 1D numpy array or 1D list. Error from data 
-    observation. For SNIa, it would be the error on the 
+    y_err : 1D numpy array or 1D list. Error from data
+    observation. For SNIa, it would be the error on the
     observed flux/magnitude.
-
 
     output : Cov. 2D numpy array, shape = (len(x),len(x))
     """
     if y_err is None:
-        y_err = N.zeros_like(x)
-        
-    A = x-x[:,None]
-    Cov = (hyperparameter[0]**2)*N.exp(-0.5*((A*A)/(hyperparameter[1]**2)))
-    Cov += (N.eye(len(y_err))*(y_err**2+floor**2+nugget**2))
-        
-    return Cov
+        y_err = np.zeros_like(x)
 
+    if new_x is None:
+        x2 = x
+        add_error = True 
+    else:
+        x2 = new_x
+        add_error = False
 
-def compute_rbf_1d_ht_matrix(new_x,old_x,hyperparameters,as_the_same_grid=False,SPEED=False):
+    A = x - x2[:,None]
+    cov = (hyperparameter[0]**2) * np.exp(-0.5 * ((A * A) / (hyperparameter[1]**2)))
     
-    HT=[]
+    if add_error:
+        cov += (np.eye(len(y_err)) * (y_err**2 + floor**2 + nugget**2))
 
-    for sn in range(len(old_x)): 
-        if as_the_same_grid:
-            New_binning=new_x[sn]
-        else:
-            New_binning=new_x
-            
-        A = old_x[sn]-New_binning[:,None]
-        HT.append((hyperparameters[0]**2)*N.exp(-0.5*((A*A)/(hyperparameters[1]**2))))
-
-    return HT
+    return cov
 
 
-def rbf_kernel_2d(x,hyperparameter,nugget,floor=0.00,y_err=None,SPEED=False):
+def rbf_kernel_2d(x, hyperparameter, new_x=None,
+                  nugget=0., floor=0.00, y_err=None):
     """
     2D RBF kernel.
 
@@ -124,43 +116,29 @@ def rbf_kernel_2d(x,hyperparameter,nugget,floor=0.00,y_err=None,SPEED=False):
     output : Cov. 2D numpy array, shape = (len(x),len(x))
     """
     if y_err is None:
-        y_err = N.zeros(len(x))
+        y_err = np.zeros(len(x))
 
-    Inv_L = N.array(([hyperparameter[2]**2,-hyperparameter[3]],
+    if new_x is None:
+        x2 = x
+        add_error = True
+    else:
+        x2 = new_x
+        add_error = False
+
+    inv_l = np.array(([hyperparameter[2]**2,-hyperparameter[3]],
                      [-hyperparameter[3],hyperparameter[1]**2]))
     
-    Inv_L *= 1./(((hyperparameter[1]**2)*(hyperparameter[2]**2))-hyperparameter[3]**2)
+    inv_l *= 1./(((hyperparameter[1]**2)*(hyperparameter[2]**2))-hyperparameter[3]**2)
     
-    A = (x[:,0]-x[:,0][:,None])
-    B = (x[:,1]-x[:,1][:,None])
+    A = (x[:,0]-x2[:,0][:,None])
+    B = (x[:,1]-x2[:,1][:,None])
 
-    Cov = (hyperparameter[0]**2)*N.exp(-0.5*(A*A*Inv_L[0,0] + 2.*A*B*Inv_L[0,1] + B*B*Inv_L[1,1]))
-    Cov += (N.eye(len(y_err))*(y_err**2+floor**2+nugget**2))
+    cov = (hyperparameter[0]**2)*np.exp(-0.5*(A * A * inv_l[0,0] + 2. * A * B * inv_l[0,1] + B * B * inv_l[1,1]))
+
+    if add_error:
+        cov += (np.eye(len(y_err))*(y_err**2+floor**2+nugget**2))
         
-    return Cov
-
-
-def compute_rbf_2d_ht_matrix(new_x,old_x,hyperparameters,as_the_same_grid=False,SPEED=False):
-
-    HT = []
-    
-    Inv_L = N.array(([hyperparameters[2]**2,-hyperparameters[3]],
-                     [-hyperparameters[3],hyperparameters[1]**2]))
-    
-    Inv_L *= 1./(((hyperparameters[1]**2)*(hyperparameters[2]**2))-hyperparameters[3]**2)
-    
-    for sn in range(len(old_x)): 
-        if as_the_same_grid:
-            New_binning = new_x[sn]
-        else:
-            New_binning = new_x
-        
-        A = (old_x[sn][:,0]-New_binning[:,0][:,None])
-        B = (old_x[sn][:,1]-New_binning[:,1][:,None])
-
-        HT.append((hyperparameters[0]**2)*N.exp(-0.5*(A*A*Inv_L[0,0] + 2.*A*B*Inv_L[0,1] + B*B*Inv_L[1,1])))
-
-    return HT
+    return cov
 
 
 

cosmogp/mean.py
@@ -1,11 +1,13 @@
 """compute mean and interpolate mean for GP."""
 
 import scipy.interpolate as inter
-import numpy as np 
+import numpy as np
 
 
 def interpolate_mean_1d(old_binning, mean_function, new_binning):
     """
+    Interpoalte 1d function.
+
     Function to interpolate 1D mean function on the new grid
     Interpolation is done using cubic spline from scipy
 
@@ -23,7 +25,6 @@ def interpolate_mean_1d(old_binning, mean_function, new_binning):
 
     output : mean_interpolate, Mean function on the new grid (New_binning)
     """
-
     cubic_spline = inter.InterpolatedUnivariateSpline(old_binning,
                                                       mean_function)
 
@@ -34,6 +35,8 @@ def interpolate_mean_1d(old_binning, mean_function, new_binning):
 
 def interpolate_mean_2d(old_binning, mean_function, new_binning):
     """
+    Interpolate 2d function.
+
     Function to interpolate 2D mean function on the new grid
     Interpolation is done using cubic spline from scipy.
 
@@ -52,16 +55,14 @@ def interpolate_mean_2d(old_binning, mean_function, new_binning):
 
     output : mean_interpolate,  Mean function on the new grid (New_binning)
     """
-
-    tck = inter.bisplrep(old_binning[:,0], old_binning[:,1],
+    tck = inter.bisplrep(old_binning[:, 0], old_binning[:, 1],
                          mean_function, task=1)
 
     mean_interpolate = np.zeros(len(new_binning))
 
     for i in range(len(new_binning)):
-        
-        mean_interpolate[i] = inter.bisplev(new_binning[i,0],
-                                            new_binning[i,1], tck)
+        mean_interpolate[i] = inter.bisplev(new_binning[i, 0],
+                                            new_binning[i, 1], tck)
 
     return mean_interpolate
 
@@ -69,19 +70,18 @@ def interpolate_mean_2d(old_binning, mean_function, new_binning):
 def return_mean(y, x, new_x=None, mean_y=None, mean_xaxis=None, diff=None):
     """
     Substract the mean function.
+
     in order to avoid systematic difference between
     average function and the data
     """
-
     if mean_y is not None:
         assert mean_xaxis is not None, 'you should provide an x axis for the average'
-        assert len(mean_y) == len(mean_xaxis), 'mean_y and mean_xaxis should have the same len' 
-        
+        assert len(mean_y) == len(mean_xaxis), 'mean_y and mean_xaxis should have the same len'
+
         if type(x[0]) is np.float64:
             mean_y_shape = interpolate_mean_1d(mean_xaxis, mean_y, x)
         else:
             mean_y_shape = interpolate_mean_2d(mean_xaxis, mean_y, x)
-        
     else:
         mean_y_shape = 0
 
@@ -89,9 +89,8 @@ def return_mean(y, x, new_x=None, mean_y=None, mean_xaxis=None, diff=None):
         y0 = mean_y_shape + np.mean(y - mean_y_shape)
     else:
         y0 = mean_y_shape + diff
-        
+
     if new_x is not None:
-        
         if mean_y is not None:
             if type(x[0]) is np.float64:
                 new_y0 = interpolate_mean_1d(x, y0, new_x)
@@ -99,8 +98,8 @@ def return_mean(y, x, new_x=None, mean_y=None, mean_xaxis=None, diff=None):
                 new_y0 = interpolate_mean_2d(x, y0, new_x)
         else:
             new_y0 = y0
-            
+
         return new_y0
-    
+
     else:
         return y0

cosmogp/pull.py
@@ -62,12 +62,16 @@ class build_pull:
                 else:
                     yerr = self.y_err[sn]
 
+                if self.y_mean is None and substract_mean:
+                    self.y_mean = np.ones_like(self.y[sn]) * np.mean(self.y[sn])
+                    self.x_axis_mean = self.x[sn] 
+                    
                 gpp = cosmogp.gaussian_process(self.y[sn][filter_pull],
                                                self.x[sn][filter_pull],
                                                y_err=yerr[filter_pull],
                                                Mean_Y=self.y_mean,
                                                Time_mean=self.x_axis_mean,
-                                               kernel=self.kernel,diff=diff,
+                                               kernel=self.kernel, diff=diff,
                                                substract_mean=substract_mean)
 
                 gpp.hyperparameters = self.hyperparameters
@@ -76,7 +80,6 @@ class build_pull:
 
                 pred[t] = gpp.Prediction[0][t]
                 pred_var[t] = abs(gpp.covariance_matrix[0][t, t])
-
 
             pull = (pred - self.y[sn])
             pull /= np.sqrt(yerr**2 + pred_var + self.nugget**2)